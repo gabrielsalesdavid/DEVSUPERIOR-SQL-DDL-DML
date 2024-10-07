5fb36bdc75fc69b9a389e64b484ce31d41e8bc72
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-10-07 20:34:27 -0300

    Create and Drop Table

---
+++ SQL-DDL.sql
@@ -0,0 +1,51 @@
+/*DDL(
+    CREATE,
+    ALTER,
+    DROP
+)*/
+
+CREATE TABLE tb_cargo(
+    id INT PRIMARY KEY,
+    nome VARCHAR(20) NOT NULL,
+    nivel VARCHAR(10),
+    salario FLOAT
+);
+
+CREATE TABLE tb_departamento(
+    id INT PRIMARY KEY,
+    nome VARCHAR(20) NOT NULL,
+    sigla VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE tb_funcionario(
+    id INT,
+    nome VARCHAR(30) NOT NULL,
+    data_adm DATE,
+    sexo CHAR(1),
+    cod_cargo INT NOT NULL,
+    cod_depto INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(cod_cargo) REFERENCES tb_cargo(id),
+    FOREIGN KEY(cod_depto) REFERENCES tb_departamento(id)
+);
+
+DROP TABLE tb_funcionario;
+
+/*DML(
+    SELECT,
+    INSERT,
+    UPDATE,
+    DELETE,
+    CALL
+)*/
+
+/*DCL(
+    GRANT,
+    REVOKE
+)*/
+
+/*TCL(
+    COMMIT,
+    SAVEPOINT,
+    ROLLBACK
+)*/
