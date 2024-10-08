bd65e076039ce1deab44aff724c1895267e9faa3
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-10-07 21:04:53 -0300

    Alter with Type Column, Alter with Add PK and Drop Constraint PK

--- SQL-DDL.sql
+++ SQL-DDL.sql
@@ -33,6 +33,25 @@ DROP TABLE tb_funcionario;
 
 ALTER TABLE tb_funcionario ADD data_nasc DATE;
 
+ALTER TABLE tb_funcionario ADD contact_email VARCHAR(20) NOT NULL;
+
+ALTER TABLE tb_funcionario DROP COLUMN data_nasc;
+
+ALTER TABLE tb_funcionario RENAME contact_email TO email;
+
+ALTER TABLE tb_funcionario ALTER COLUMN email TYPE VARCHAR(20);
+
+/*Exemple*/
+
+CREATE TABLE table_exemple(
+    id INT,
+    nome VARCHAR(40)
+);
+
+ALTER TABLE table_exemple ADD PRIMARY KEY(id);
+
+ALTER TABLE table_exemple DROP CONSTRAINT table_exemple_pkey;
+
 /*DML(
     SELECT,
     INSERT,
