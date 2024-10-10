c71cede3169433bb22030188cfa7d4b632b3e428
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-10-10 18:41:30 -0300

    Create Table Rede Social

---
+++ SQL-DDL-Exercicio-Evento-Rede-Social.sql
@@ -0,0 +1,45 @@
+CREATE TABLE tb_usuario(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    nascimento DATE,
+    website VARCHAR(30),
+    genero CHAR(1) NOT NULL,
+    telefone VARCHAR(20),
+    foto_perfil_id INT,
+    FOREIGN KEY(foto_perfil_id) REFERENCES tb_foto(id)
+);
+
+CREATE TABLE tb_seguidores(
+    seguidor_id INT,
+    seguido_id INT,
+    PRIMARY KEY(seguidor_id, seguido_id),
+    FOREIGN KEY(seguidor_id) REFERENCES tb_usuario(id),
+    FOREIGN KEY(seguido_id) REFERENCES tb_usuario(id)
+);
+
+CREATE TABLE tb_postagem(
+    id SERIAL PRIMARY KEY,
+    texto VARCHAR(20) NOT NULL,
+    istante TIMESTAMP NOT NULL,
+    autor_id INT,
+    FOREIGN KEY(autor_id) REFERENCES tb_usuario(id)
+);
+
+CREATE TABLE tb_album(
+    id SERIAL PRIMARY KEY,
+    isntante_postagem TIMESTAMP NOT NULL,
+    usuario_id INT,
+    FOREIGN KEY(usuarui_id) REFERENCES tb_usuario(id)
+);
+
+CREATE TABLE tb_foto(
+    uri VARCHAR(30) PRIMARY KEY NOT NULL,
+    instante_postagem TIMESTAMP NOT NULL,
+    postagem_id INT,
+    usuario_id INT,
+    album_id INT,
+    FOREIGN KEY(postagem_id) REFERENCES tb_postagem(id),
+    FOREIGN KEY(usuario_id) REFERENCES tb_usuario(id),
+    FOREIGN KEY(album_id) REFERENCES tb_album(id)
+);
