9c94abcc4e28c96293e5f93bf7fb139aa01c7f08
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-10-10 12:47:49 -0300

    DDL Script Correction

--- SQL-DDL-Exercicio-Evento.sql
+++ SQL-DDL-Exercicio-Evento.sql
@@ -1,29 +1,30 @@
 /* DDL */
 
 CREATE TABLE tb_local_nascimento(
-    codigo INT PRIMARY KEY,
+    codigo SERIAL PRIMARY KEY,
     cidade VARCHAR(10) NOT NULL,
     uf CHAR(2)
 );
 
 CREATE TABLE tb_aluno(
-    codigo INT PRIMARY KEY,
+    codigo SERIAL PRIMARY KEY,
     nome VARCHAR(20) NOT NULL,
     cod_nasc INT NOT NULL,
     FOREIGN KEY(cod_nasc) REFERENCES tb_local_nascimento(id)
 );
 
 CREATE TABLE tb_disciplina(
-    codigo INT PRIMARY KEY,
+    codigo SERIAL PRIMARY KEY,
     nome VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE tb_matricula(
-    cod_aluno INT NOT NULL,
-    cod_turma INT PRIMARY KEY,
-    cod_disc INT NOT NULL,
-    FOREIGN KEY(cod_aluno) REFERENCES tb_aluno(id),
-    FOREIGN KEY(cod_disc) REFERENCES tb_disciplina(id)
+CREATE TABLE tb_participacao(
+    cod_aluno INT,
+    cod_turma INT,
+    cod_disc,
+	PRIMARY KEY(cod_aluno, cod_turma, cod_disc),
+    FOREIGN KEY(cod_aluno) REFERENCES tb_aluno(codigo),
+    FOREIGN KEY(cod_disc) REFERENCES tb_disciplina(codigo),
 );
 
 /* Ateração de dados da tabela do tipo caracter de tamanho 20 */
