a1e79c437b782137b5c64a6511eb4ac30d11e84e
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-10-11 09:59:23 -0300

    SCRIPT CORRECTION INSERT!

--- SQL-DML-Exercicio-Rede-Social.sql
+++ SQL-DML-Exercicio-Rede-Social.sql
@@ -15,5 +15,5 @@ INSERT INTO tb_postagem(id, texto, instante, autor_id) VALUES (2, 'Bom dia pesso
 
 INSERT INTO tb_album(id, titulo, instante_postagem, usuario_id) VALUES (1, 'Férias de verão', TIMESTAMP WITH TIME ZONE '2017-01-13T16:50:13Z', 3);
 
-INSERT INTO tb_foto(uri, instante_postagem, postagem_id, usuario_id, album_id) VALUES ('http://dominio.com/praia2.jpg', TIMESTAMP WITH TIME ZONE '2017-01-10T13:30:14Z', NULL, 3, 1);
+INSERT INTO tb_foto(uri, instante_postagem, postagem_id, usuario_id, album_id) VALUES ('http://dominio.com/praia1.jpg', TIMESTAMP WITH TIME ZONE '2017-01-10T13:30:14Z', NULL, 3, 1);
 INSERT INTO tb_foto(uri, instante_postagem, postagem_id, usuario_id, album_id) VALUES ('http://dominio.com/praia2.jpg', TIMESTAMP WITH TIME ZONE '2017-01-10T13:31:12Z', NULL, 3, 1);
